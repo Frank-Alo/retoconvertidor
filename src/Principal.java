a9900846a1a40a358bf8c63e9299c138fbfb5c4a
Frank-Alo <frk8006@gmail.com>
2024-05-02 16:51:21 -0500

    metodo convertidor

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,63 @@
-public class Principal {
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.util.Scanner;
+
+public class Principal extends convertidor {
+    public Principal() {
+    }
 
     public static void main(String[] args) {
-        System.out.println("probando");
+        String strJson = consumeURL("https://v6.exchangerate-api.com/v6/a0e129a4d5e8fbb96affe09f/latest/USD");
+        Gson gson = new Gson();
+        JsonObject jsonObject = (JsonObject)gson.fromJson(strJson, JsonObject.class);
+        String monedaO = "";
+        String monedaD = "";
+        double valueMonedaO = 0.0;
+        double valueMonedaD = 0.0;
+        double cantidadAConvertir = 0.0;
+        Scanner scanner = new Scanner(System.in);
+        boolean reintentar = true;
+
+        while(reintentar) {
+            System.out.println("*******************************************");
+            System.out.println("Bienvenido al conversor de moneda");
+            System.out.println("Ingrese la moneda origen");
+            monedaO = scanner.next();
+            System.out.println("Ingrese la cantidad a convertir");
+            cantidadAConvertir = Double.parseDouble(scanner.next());
+            System.out.println("Ingrese la moneda destino");
+            monedaD = scanner.next();
+
+            try {
+                valueMonedaO = jsonObject.getAsJsonObject("conversion_rates").get(monedaO).getAsDouble();
+            } catch (Exception var18) {
+                monedaO = "COP";
+                valueMonedaO = jsonObject.getAsJsonObject("conversion_rates").get("COP").getAsDouble();
+            }
+
+            try {
+                valueMonedaD = jsonObject.getAsJsonObject("conversion_rates").get(monedaD).getAsDouble();
+            } catch (Exception var17) {
+                monedaD = "USD";
+                valueMonedaD = jsonObject.getAsJsonObject("conversion_rates").get("USD").getAsDouble();
+            }
+
+            System.out.println("1 " + monedaO + " equivalen a " + valueMonedaD * 1.0 / valueMonedaO + " " + monedaD);
+            double resultado = valueMonedaD * cantidadAConvertir / valueMonedaO;
+            System.out.println("" + cantidadAConvertir + " " + monedaO + " equivalen a " + resultado + " " + monedaD);
+            System.out.println("Desea continuar con otra conversión ? 1 para SI, 0 para NO");
+            String res = scanner.next();
+            reintentar = false;
+            if (res.equals("1")) {
+                reintentar = true;
+            }
+        }
+
+        scanner.close();
     }
 }
--- src/convertidor.java
+++ src/convertidor.java
@@ -1,8 +1,37 @@
-public class convertidor {
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
-    public void convertidor() {
-        convertidor convertidor = new convertidor();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class convertidor {
+    public convertidor() {
     }
 
+    public static String consumeURL(String urlString) {
+        StringBuilder response = new StringBuilder();
 
+        try {
+            URL url = new URL(urlString);
+            URLConnection conexion = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+
+            String inputLine;
+            while((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            in.close();
+        } catch (IOException var6) {
+            IOException e = var6;
+            e.printStackTrace();
+        }
+
+        return response.toString();
+    }
 }
